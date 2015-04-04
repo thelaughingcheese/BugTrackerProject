98bcb13b3eb9eb5f6d8830be4b1b3a48aa2b3095
thelaughingcheese <jiasheng95@yahoo.ca>
2015-04-04 16:08:24 -0400

    notifications work now!
    email account:
    cps406@mail.com
    asdfasdf

--- notificationManager.java
+++ notificationManager.java
@@ -12,39 +12,50 @@ public class notificationManager{
 			acc.setUsername(subs.get(i));
 		
 			//send email
-			System.out.println("send email to "+acc.getEmail());
+			String to = acc.getEmail();
+			String from = "cps406@mail.com";
+			String pass = "asdfasdf";
+			String host = "smtp.mail.com";
 			
-			  String to = "abcd@gmail.com";
-			  String from = "BugTracker@gmail.com";
-			  String host = "localhost";
+			Properties properties = System.getProperties();
+			properties.put("mail.smtp.starttls.enable", "true");
+			properties.put("mail.smtp.host", host);
+			properties.put("mail.smtp.user", from);
+			properties.put("mail.smtp.password", pass);
+			properties.put("mail.smtp.port", "587");
+			properties.put("mail.smtp.auth", "true");
 
-			  Properties properties = System.getProperties();
-			  properties.setProperty("mail.smtp.host", host);
-			  Session session = Session.getDefaultInstance(properties);
+			Session session = Session.getDefaultInstance(properties);
 
-			  try{
-				 // Create a default MimeMessage object.
-				 MimeMessage message = new MimeMessage(session);
+			try{
+			 // Create a default MimeMessage object.
+			 MimeMessage message = new MimeMessage(session);
 
-				 // Set From: header field of the header.
-				 message.setFrom(new InternetAddress(from));
+			 // Set From: header field of the header.
+			 message.setFrom(new InternetAddress(from));
 
-				 // Set To: header field of the header.
-				 message.addRecipient(Message.RecipientType.TO,
-										  new InternetAddress(to));
+			 // Set To: header field of the header.
+			 message.addRecipient(Message.RecipientType.TO,
+									  new InternetAddress(to));
 
-				 // Set Subject: header field
-				 message.setSubject("This is the Subject Line!");
+			 // Set Subject: header field
+			 message.setSubject(msg);
 
-				 // Now set the actual message
-				 message.setText("This is actual message");
+			 // Now set the actual message
+			 message.setText(msg);
 
-				 // Send message
-				 Transport.send(message);
-				 System.out.println("Sent message successfully....");
-			  }catch (MessagingException mex) {
-				 mex.printStackTrace();
-			  }
+			 // Send message
+			 //Transport.send(message);
+			 
+			 Transport transport = session.getTransport("smtp");
+				transport.connect(host, from, pass);
+				transport.sendMessage(message, message.getAllRecipients());
+				transport.close();
+			 
+			 System.out.println("Sent message successfully....");
+			}catch (MessagingException mex) {
+			 mex.printStackTrace();
+			}
 		}
 	}
 }
--- testEmail.java
+++ testEmail.java
@@ -8,13 +8,17 @@ public class testEmail{
 	public static void main(String args[]){
 
 		String to = "jason.jiasheng.yu@ryerson.ca";
-		String from = "BugTracker@gmail.com";
-		String host = "localhost";
+		String from = "cps406@mail.com";
+		String pass = "asdfasdf";
+		String host = "smtp.mail.com";
 		
-	System.out.println("1");
 		Properties properties = System.getProperties();
-
-		properties.setProperty("mail.smtp.host", host);
+		properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.user", from);
+        properties.put("mail.smtp.password", pass);
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.auth", "true");
 
 		Session session = Session.getDefaultInstance(properties);
 
@@ -36,7 +40,11 @@ public class testEmail{
 		 message.setText("This is actual message");
 
 		 // Send message
-		 Transport.send(message);
+		 Transport transport = session.getTransport("smtp");
+            transport.connect(host, from, pass);
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+		 
 		 System.out.println("Sent message successfully....");
 		}catch (MessagingException mex) {
 		 mex.printStackTrace();
