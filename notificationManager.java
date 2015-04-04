81207befde508cd0b87ce560fb2a6d1a09ea2613
thelaughingcheese <jiasheng95@yahoo.ca>
2015-04-04 16:11:04 -0400

    slight refactoring for email sending

--- notificationManager.java
+++ notificationManager.java
@@ -12,50 +12,53 @@ public class notificationManager{
 			acc.setUsername(subs.get(i));
 		
 			//send email
-			String to = acc.getEmail();
-			String from = "cps406@mail.com";
-			String pass = "asdfasdf";
-			String host = "smtp.mail.com";
-			
-			Properties properties = System.getProperties();
-			properties.put("mail.smtp.starttls.enable", "true");
-			properties.put("mail.smtp.host", host);
-			properties.put("mail.smtp.user", from);
-			properties.put("mail.smtp.password", pass);
-			properties.put("mail.smtp.port", "587");
-			properties.put("mail.smtp.auth", "true");
+			notificationManager.sendEmail(acc.getEmail(),msg,msg);
+		}
+	}
+	
+	public static void sendEmail(String to,String title,String msg){
+		String from = "cps406@mail.com";
+		String pass = "asdfasdf";
+		String host = "smtp.mail.com";
+		
+		Properties properties = System.getProperties();
+		properties.put("mail.smtp.starttls.enable", "true");
+		properties.put("mail.smtp.host", host);
+		properties.put("mail.smtp.user", from);
+		properties.put("mail.smtp.password", pass);
+		properties.put("mail.smtp.port", "587");
+		properties.put("mail.smtp.auth", "true");
 
-			Session session = Session.getDefaultInstance(properties);
+		Session session = Session.getDefaultInstance(properties);
 
-			try{
-			 // Create a default MimeMessage object.
-			 MimeMessage message = new MimeMessage(session);
+		try{
+		 // Create a default MimeMessage object.
+		 MimeMessage message = new MimeMessage(session);
 
-			 // Set From: header field of the header.
-			 message.setFrom(new InternetAddress(from));
+		 // Set From: header field of the header.
+		 message.setFrom(new InternetAddress(from));
 
-			 // Set To: header field of the header.
-			 message.addRecipient(Message.RecipientType.TO,
-									  new InternetAddress(to));
+		 // Set To: header field of the header.
+		 message.addRecipient(Message.RecipientType.TO,
+								  new InternetAddress(to));
 
-			 // Set Subject: header field
-			 message.setSubject(msg);
+		 // Set Subject: header field
+		 message.setSubject(title);
 
-			 // Now set the actual message
-			 message.setText(msg);
+		 // Now set the actual message
+		 message.setText(msg);
 
-			 // Send message
-			 //Transport.send(message);
-			 
-			 Transport transport = session.getTransport("smtp");
-				transport.connect(host, from, pass);
-				transport.sendMessage(message, message.getAllRecipients());
-				transport.close();
-			 
-			 System.out.println("Sent message successfully....");
-			}catch (MessagingException mex) {
-			 mex.printStackTrace();
-			}
+		 // Send message
+		 //Transport.send(message);
+		 
+		 Transport transport = session.getTransport("smtp");
+			transport.connect(host, from, pass);
+			transport.sendMessage(message, message.getAllRecipients());
+			transport.close();
+		 
+		 System.out.println("Sent message successfully....");
+		}catch (MessagingException mex) {
+		 mex.printStackTrace();
 		}
 	}
 }
